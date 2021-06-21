212c63c826cc1e1c559d7dcb798955f199abff54
Guilerme Barciki <guilermebarciki@gmail.com>
2021-06-21 11:01:28 -0300

    arrumei bug do asterisco

--- Jade-Mainframe/Views/Components/TextFieldView.swift
+++ Jade-Mainframe/Views/Components/TextFieldView.swift
@@ -27,7 +27,7 @@ struct TextFieldView: View {
     // Title optionally adds a visual/textual indicator that the field is required and must be filled
     private var fieldTitle: some View {
         // TODO: Add verification for doubled asterisks in case `title` already comes with one
-        Text(title + (isRequired ? "*" : ""))
+        Text(title + (isRequired && title.last! != "*" ? "*" : ""))
             .font(.system(.body, design: .default)
                     .weight(.bold))
     }
