075eb9bbecb7cfac0122bfc9cc2c0b6c11fa66cc
Guilerme Barciki <guilermebarciki@gmail.com>
2021-06-23 10:11:02 -0300

    refatoraçao CarouselView + arquivos nao usados deletados

--- Jade-Mainframe/Views/Components/CarouselView.swift
+++ Jade-Mainframe/Views/Components/CarouselView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 private struct Product: Identifiable {
     var id = UUID()
-    let productName, imageUrl: String
+    let productName, imageName: String
     
 }
 
@@ -18,52 +18,54 @@ struct CarouselView: View {
     // associar isso ao banco de dados
     // os parametros ainda devem ser decididos
    private let products: [Product] = [
-    Product( productName: "0", imageUrl: "0"),
-    Product( productName: "0fe", imageUrl: "0"),
-    Product( productName: "0fef", imageUrl: "0"),
-    Product( productName: "0fef", imageUrl: "0"),
-    Product( productName: "0fef", imageUrl: "0"),
-    Product( productName: "0fef", imageUrl: "0"),
-    Product( productName: "0fef", imageUrl: "0"),
-    Product( productName: "0fef", imageUrl: "0"),
-    Product( productName: "0fef", imageUrl: "0"),
+    Product( productName: "0", imageName: "0"),
+    Product( productName: "0fe", imageName: "0"),
+    Product( productName: "0fef", imageName: "0"),
+    Product( productName: "0fef", imageName: "0"),
+    Product( productName: "0fef", imageName: "0"),
+    Product( productName: "0fef", imageName: "0"),
+    Product( productName: "0fef", imageName: "0"),
+    Product( productName: "0fef", imageName: "0"),
+    Product( productName: "0fef", imageName: "0"),
     ]
     let categorieTitle = "titulo"
  
     var body: some View { 
-        VStack(alignment: .leading){
+        VStack(alignment: .leading) {
             
             Text(categorieTitle)
                 .font(.largeTitle)
                 .fontWeight(.bold)
                 .multilineTextAlignment(.leading)
             
-            ScrollView(.horizontal){
-                HStack(spacing: 20){
-                    ForEach(products){ item in
-                        ProductMiniView(productIdentification: item.productName, imageUrl: item.imageUrl)
+            ScrollView(.horizontal) {
+                HStack(spacing: LayoutMetrics.horizontalSpacing) {
+                    ForEach(products) { product in
+                        productMiniView(product: product)
                     }
                 }
             }
         }
-        
     }
+    
+    private func productMiniView(product: Product) -> some View {
+        VStack {
+            Image(product.imageName)
+                .frame(width: LayoutMetrics.miniProductWidth, height: LayoutMetrics.miniProductHeight)
+            
+            Text(product.productName)
+        }
+    }
+    
+    private enum LayoutMetrics {
+        static let horizontalSpacing: CGFloat = 20
+        static let miniProductWidth: CGFloat = 100
+        static let miniProductHeight: CGFloat = 100
+    }
+}
 
 struct CarouselView_Previews: PreviewProvider {
     static var previews: some View {
         CarouselView()
     }
-}
-
-struct ProductMiniView : View {
-    let productIdentification: String
-    let imageUrl: String
-    var body: some View{
-        VStack{
-        // Seria Image(imageUrl) ao inves do retangulo
-            Image(systemName: "").frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/).background(Color(.black))
-            Text(productIdentification)
-        }
-    }
-}
 }
