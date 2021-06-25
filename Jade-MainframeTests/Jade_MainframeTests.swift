2a68ad26cdc3525679d3456da9eff27435835695
Vinícius Couto <vcoutasso@gmail.com>
2021-06-25 14:30:39 -0300

    Remove performance test

--- Jade-MainframeTests/Jade_MainframeTests.swift
+++ Jade-MainframeTests/Jade_MainframeTests.swift
@@ -20,11 +20,4 @@ class Jade_MainframeTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
 }
