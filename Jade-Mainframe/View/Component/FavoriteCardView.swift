2c0b7fe06f233ac8d50bee089a33c58a4c55bb45
Guilerme Barciki <guilermebarciki@gmail.com>
2021-06-30 19:25:28 -0300

    coloquei LayoutMetrics

--- Jade-Mainframe/View/Component/FavoriteCardView.swift
+++ Jade-Mainframe/View/Component/FavoriteCardView.swift
@@ -13,7 +13,9 @@ struct FavoriteCardView: View {
     let phonePrice: Double
     var body: some View {
         HStack(spacing: 20) {
-            Rectangle().frame(width: 115, height: 115).cornerRadius(10).offset(x: -10)
+            Rectangle().frame(width: LayoutMetrics.cardSize, height: LayoutMetrics.cardSize)
+                .cornerRadius(LayoutMetrics.cornerRadius)
+                .offset(x: LayoutMetrics.cardOffSet)
             VStack(alignment: .leading, spacing: 10) {
                 Text("\(phoneModel) \(phoneSpace)")
                 Text("R$\(priceFormatter(price: phonePrice, locale: Strings.locale))")
@@ -21,12 +23,12 @@ struct FavoriteCardView: View {
                     Spacer()
                     heartIcon
                     binocularIconStroke
-                }.offset(x: 10)
+                }.offset(x: LayoutMetrics.iconsOffSet)
             }.padding(.top)
-                .offset(x: -10)
+                .offset(x: LayoutMetrics.cardOffSet)
         }.padding(10)
-            .background(RoundedRectangle(cornerRadius: 10)
-                .frame(width: 376, height: 129, alignment: .center)
+            .background(RoundedRectangle(cornerRadius: LayoutMetrics.cornerRadius)
+                .frame(width: LayoutMetrics.cardWidth, height: LayoutMetrics.cardHeight, alignment: .center)
                 .foregroundColor(Color(.systemGray6)))
             .padding([.leading, .trailing])
     }
@@ -41,6 +43,18 @@ struct FavoriteCardView: View {
     private var heartIcon: some View {
         return Image(systemName: "heart.circle.fill").font(.system(size: 38))
             .foregroundColor(Color(Assets.Colors.TecoPalette.darkBlue.color))
+    }
+
+    // MARK: - Layout Metrics
+
+    private enum LayoutMetrics {
+        static let cardSize: CGFloat = 115
+        static let cornerRadius: CGFloat = 10
+        static let cardWidth: CGFloat = 376
+        static let cardHeight: CGFloat = 129
+        static let cardOffSet: CGFloat = -10
+        static let iconsOffSet: CGFloat = 10
+        static let cardPadding: CGFloat = 3
     }
 }
 
