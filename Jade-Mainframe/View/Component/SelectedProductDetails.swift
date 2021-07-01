d7f90d42fec477799636feed9f72a4f9ca89680a
Guilerme Barciki <guilermebarciki@gmail.com>
2021-07-01 10:10:37 -0300

    Falta ajustar as fontes

--- Jade-Mainframe/View/Component/SelectedProductDetails.swift
+++ Jade-Mainframe/View/Component/SelectedProductDetails.swift
@@ -11,6 +11,9 @@ struct SelectedProductDetails: View {
     // MARK: - Variables
 
     @ObservedObject var viewModel: SelectedProductDetailsManager
+
+    // MARK: - body
+
     var body: some View {
         ScrollView {
             VStack(alignment: .leading) {
@@ -22,8 +25,10 @@ struct SelectedProductDetails: View {
         }
     }
 
+    // MARK: - Private variables
+
     private var favoriteIcon: some View {
-        return Image(systemName: "heart.circle").font(.system(size: 38))
+        return Image(Assets.Images.Icons.heart.name).font(.system(size: 38))
             .foregroundColor(Color(Assets.Colors.TecoPalette.darkBlue.color))
     }
 
@@ -34,7 +39,13 @@ struct SelectedProductDetails: View {
                 favoriteIcon
             }
             VStack(alignment: .leading) {
-                Text("\(priceFormatter(price: viewModel.product.productDiscount, locale: Strings.locale))")
+                Text("\(priceFormatter(price: viewModel.product.productPrice - viewModel.product.productDiscount, locale: Strings.locale))")
+                    .strikethrough()
+                    .foregroundColor(Color(Assets
+                            .Colors
+                            .TecoPalette
+                            .mediumDarkGray
+                            .color))
                 Text("\(priceFormatter(price: viewModel.product.productPrice, locale: Strings.locale))")
             }
         }
@@ -43,48 +54,90 @@ struct SelectedProductDetails: View {
     private var productDescription: some View {
         VStack(alignment: .leading) {
             Text("Descrição")
-            Text("blabla") // precisa colocar este parametro no Product
+            Text("blabljhbhjkl \n hba").foregroundColor(Color(Assets
+                    .Colors
+                    .TecoPalette
+                    .mediumGray
+                    .color)) // precisa colocar este parametro no Product
         }
     }
 
     private var aboutProduct: some View {
-        let lines: [IconAndText] = [IconAndText(systemName: "clock.arrow.circlepath", text: "Tempo de uso:"),
-                                    IconAndText(systemName: "doc.text", text: "Nota fiscal:"),
-                                    IconAndText(systemName: "airpodspro", text: "Acessórios:"),
-                                    IconAndText(systemName: "iphone", text: "Precisando troca de tela:")]
-        return VStack(alignment: .leading) {
+        let lines: [IconAndText] = [
+            IconAndText(systemName: "clock.arrow.circlepath", text: "Tempo de uso:"),
+            IconAndText(systemName: "doc.text", text: "Nota fiscal:"),
+            IconAndText(systemName: "airpodspro", text: "Acessórios:"),
+        ]
+        return VStack(alignment: .leading, spacing: 0) {
             Text("Sobre este aparelho")
             ForEach(lines) { line in
                 HStack(spacing: 5) {
                     VStack {
-                        Image(systemName: line.systemName).frame(width: 25, height: 30, alignment: .center)
+                        Image(systemName: line.systemName)
+                            .frame(width: 25, height: 30, alignment: .center)
+                            .foregroundColor(Color(Assets
+                                    .Colors
+                                    .TecoPalette.mediumDarkGray
+                                    .color))
                     }
                     VStack(alignment: .leading) {
                         Text(line.text)
+                            .foregroundColor(Color(Assets
+                                    .Colors
+                                    .TecoPalette.mediumDarkGray
+                                    .color))
                     }
                 }
+            }
+            HStack {
+                Image(Assets.Images.Icons.iphoneScreen.name).frame(width: 25, height: 30, alignment: .center)
+                Text("Precisando troca de tela:")
+                    .foregroundColor(Color(Assets
+                            .Colors
+                            .TecoPalette.mediumDarkGray
+                            .color))
             }
         }
     }
 
     private var technicalSheet: some View {
-        let lines: [IconAndText] = [IconAndText(systemName: "clock.arrow.circlepath", text: "Tempo de uso:"),
-                                    IconAndText(systemName: "doc.text", text: "Nota fiscal:"),
-                                    IconAndText(systemName: "cpu", text: "Memória interna:"),
-                                    IconAndText(systemName: "cpu", text: "Memória RAM:")]
+        let lines: [IconAndText] = [
+            IconAndText(systemName: "clock.arrow.circlepath", text: "Tempo de uso:"),
+            IconAndText(systemName: "doc.text", text: "Nota fiscal:"),
+            IconAndText(systemName: "cpu", text: "Memória interna:"),
+            IconAndText(systemName: "cpu", text: "Memória RAM:"),
+        ]
         return VStack(alignment: .leading) {
             Text("Sobre este aparelho")
             ForEach(lines) { line in
                 HStack(spacing: 5) {
                     VStack {
-                        Image(systemName: line.systemName).frame(width: 25, height: 30, alignment: .center)
+                        Image(systemName: line.systemName)
+                            .frame(width: 25, height: 30, alignment: .center)
+                            .foregroundColor(Color(Assets
+                                    .Colors
+                                    .TecoPalette
+                                    .mediumDarkGray
+                                    .color))
                     }
                     VStack(alignment: .leading) {
                         Text(line.text)
+                            .foregroundColor(Color(Assets
+                                    .Colors
+                                    .TecoPalette.mediumDarkGray
+                                    .color))
                     }
                 }
             }
         }
+    }
+
+    // MARK: - Struct
+
+    struct IconAndText: Identifiable {
+        var id = UUID()
+        let systemName: String
+        let text: String
     }
 
     struct SelectedProductDetails_Previews: PreviewProvider {
@@ -93,10 +146,4 @@ struct SelectedProductDetails: View {
             SelectedProductDetails(viewModel: mockViewModel)
         }
     }
-}
-
-struct IconAndText: Identifiable {
-    var id = UUID()
-    let systemName: String
-    let text: String
 }
