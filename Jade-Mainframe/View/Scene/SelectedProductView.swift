fce51830cc88e6d42391f556dd99db7cd767ba1c
Guilerme Barciki <guilermebarciki@gmail.com>
2021-06-30 21:32:34 -0300

    so criei o arquivo

---
+++ Jade-Mainframe/View/Scene/SelectedProductView.swift
@@ -0,0 +1,20 @@
+//
+//  SelectedProductView.swift
+//  Jade-Mainframe
+//
+//  Created by Guilerme Barciki on 30/06/21.
+//
+
+import SwiftUI
+
+struct SelectedProductView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SelectedProductView_Previews: PreviewProvider {
+    static var previews: some View {
+        SelectedProductView()
+    }
+}
