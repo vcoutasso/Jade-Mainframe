3bf59ef54879c6c09b718cd9c4caa1505f6162f4
Guilerme Barciki <guilermebarciki@gmail.com>
2021-07-01 21:10:32 -0300

    arrumei + 1 coisa

--- Teco/Generated/Strings.swift
+++ Teco/Generated/Strings.swift
@@ -22,7 +22,7 @@ internal enum Strings {
   internal static let announcedProducts = Strings.tr("Localizable", "announcedProducts")
   /// Compra e Venda
   internal static let buyAndSell = Strings.tr("Localizable", "buyAndSell")
-  /// Cansado de procurar? \nA gente fica de olho e te avisa \n quando achar.
+  /// Cansado de procurar? \nA gente fica de olho e te avisa \nquando achar.
   internal static let callToTheWatchList = Strings.tr("Localizable", "callToTheWatchList")
   /// R$
   internal static let currencySymbol = Strings.tr("Localizable", "currencySymbol")
