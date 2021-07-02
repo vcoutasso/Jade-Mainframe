fbe6e41035f29d29ef64b4fca98da6ec337d05ad
Vinícius Couto <vcoutasso@gmail.com>
2021-07-02 00:35:50 -0300

    Fix

--- Teco/View/Scene/AppView.swift
+++ Teco/View/Scene/AppView.swift
@@ -26,11 +26,7 @@ struct AppView: View {
                 Image(systemName: tabBarManager.homeSymbolName)
                 Text(Strings.homeTabName)
             }
-<<<<<<< HEAD
             SellingView().tabItem {
-=======
-            NotFoundView().tabItem {
->>>>>>> e3caaa768dea105d55ecad55e1a770baa1b82776
                 Image(systemName: tabBarManager.sellingSymbolName)
                 Text(Strings.sellingTabName)
             }
